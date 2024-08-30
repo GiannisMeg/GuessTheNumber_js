a3d3ad458b8eabb7f370702d501b54fb0e7bcdc5
Giannis Megoulas <95855227+GiannisMeg@users.noreply.github.com>
2024-08-30 11:48:13 +0200

    basic game logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+// document.querySelector(".message").textContent = "Correct Number";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// //get input value
+// document.querySelector(".guess").value;
+
+//define secret number outside the event handler
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// score called state variable cause is part of the application state
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+	// the return value is gonna be a string so we have to turn it to a number in case we want to compare it
+	// const guess = document.querySelector(".guess").value;
+	const guess = Number(document.querySelector(".guess").value);
+
+	// in application like this we have to check if there is a value as first scenario
+	if (!guess) {
+		document.querySelector(".message").textContent = "⛔️ No number!";
+	}
+});
